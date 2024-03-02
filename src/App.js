0b3340eceee77efc7d3619a1b536fff70e2c0225
Taran234 <ts406093@gmail.com>
2024-03-02 17:27:43 -0500

    privacy-policy

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
 import Resume from "./components/Resume/ResumeNew";
+import Privacy from "./components/Privacy/Privacy";
+
 import {
   BrowserRouter as Router,
   Route,
@@ -39,7 +41,8 @@ function App() {
           <Route path="/project" element={<Projects />} />
           <Route path="/about" element={<About />} />
           <Route path="/resume" element={<Resume />} />
-          <Route path="*" element={<Navigate to="/"/>} />
+          <Route path="/privacy-policy" element={<Privacy />} />
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
         <Footer />
       </div>
